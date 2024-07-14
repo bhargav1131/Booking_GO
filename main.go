633dec07a5fabfa15913b3ac287ecb2fbc999dd5
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-14 12:09:38 +0530

    add first and last name and use array for tracking users

--- main.go
+++ main.go
@@ -6,21 +6,30 @@ import (
 )
 
 func main() {
-	var conferenceName = "My Conference"
+	conferenceName := "My Conference"
 	const conferenceTickets = 30
 	var remainingTickets = 30
+	// var bookings [50]string //array
+	var bookings []string //slices are dynamic
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("Total available tickets is %v  and remaining tickets are %v \n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend our conference..")
 
-	var userName string
+	var firstName string
+	var lastName string
 	var userTickets int
+
 	fmt.Println()
-	fmt.Println("Enter your name: ")
-	fmt.Scan(&userName)
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
 	fmt.Println("Enter the number of tickets you want to book ")
 	fmt.Scan(&userTickets)
+
+	// bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName+" "+lastName)
 
 	if userTickets > conferenceTickets || userTickets > remainingTickets {
 		fmt.Println("Invalid input")
@@ -30,7 +39,9 @@ func main() {
 		remainingTickets = remainingTickets - userTickets
 	}
 
-	fmt.Printf("Congrats %v you have successfully booked %v tickets\n", userName, userTickets)
+	fmt.Printf("Congrats %v %v! you have successfully booked %v tickets\n", firstName, lastName, userTickets)
 	fmt.Printf("Available tickets: %v\n", remainingTickets)
+
+	fmt.Println(bookings[0])
 
 }
