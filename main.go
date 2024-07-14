3d25b21475ee0cc71ccb0cba1141db25ccbb0d00
Bhargav Pratim <bhargavsarma173@gmail.com>
2024-07-14 12:44:42 +0530

    change in structure

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -12,36 +13,53 @@ func main() {
 	// var bookings [50]string //array
 	var bookings []string //slices are dynamic
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("Total available tickets is %v  and remaining tickets are %v \n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend our conference..")
+	fmt.Printf("**Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("**Total available tickets is %v  and remaining tickets are %v \n", conferenceTickets, remainingTickets)
+	fmt.Println("**Get your tickets here to attend our conference..")
 
 	var firstName string
 	var lastName string
 	var userTickets int
+	var customerID int = 1
+	var customerIndex int = 0
 
-	fmt.Println()
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
-	fmt.Println("Enter the number of tickets you want to book ")
-	fmt.Scan(&userTickets)
+	for remainingTickets != 0 {
 
-	// bookings[0] = firstName + " " + lastName
-	bookings = append(bookings, firstName+" "+lastName)
+		//user details
+		fmt.Println()
+		fmt.Printf("#ID: %v\n", customerID)
+		fmt.Println("->Enter your first name: ")
+		fmt.Scan(&firstName)
+		fmt.Println("->Enter your last name: ")
+		fmt.Scan(&lastName)
+		fmt.Println("->Enter the number of tickets you want to book ")
+		fmt.Scan(&userTickets)
+		customerID++
 
-	if userTickets > conferenceTickets || userTickets > remainingTickets {
-		fmt.Println("Invalid input")
-		return
+		// bookings[0] = firstName + " " + lastName
+		bookings = append(bookings, firstName+" "+lastName)
 
-	} else {
-		remainingTickets = remainingTickets - userTickets
+		//ticket booking logic
+		if userTickets > conferenceTickets || userTickets > remainingTickets {
+			fmt.Println("Invalid input")
+			return
+
+		} else {
+			remainingTickets = remainingTickets - userTickets
+		}
+
+		//logic to extract only the first name
+		onlyFirstName := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			onlyFirstName = append(onlyFirstName, names[0])
+		}
+
+		// welcome statements
+		fmt.Printf("*Congrats %v! You have successfully booked %v tickets for the upcoming conference\n", onlyFirstName[customerIndex], userTickets)
+		customerIndex++
+		fmt.Printf("*Current available tickets: %v\n", remainingTickets)
+		fmt.Println()
+		fmt.Println()
 	}
-
-	fmt.Printf("Congrats %v %v! you have successfully booked %v tickets\n", firstName, lastName, userTickets)
-	fmt.Printf("Available tickets: %v\n", remainingTickets)
-
-	fmt.Println(bookings[0])
-
 }
